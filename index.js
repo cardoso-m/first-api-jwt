03672f84236deba28d11346f68613d1f41e92d31
Matheus Cardoso <mdonizetecardoso@gmail.com>
2024-10-07 22:02:13 -0300

    Criação da tabela 'users' no BD

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express()
 const connection = require('./database/database')
+const userController = require('./user/userController')
 
 connection.authenticate().then(() => {
     console.log("CONN OK!")
 }).catch((MsgErr) => {
     console.log(MsgErr)
 })
+
+app.use('/', userController)
 
 app.listen(8000, () => {
     console.log("Server ON!")
