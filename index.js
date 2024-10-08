a140cbf4e058f1d2f23de075351a8783effd56f9
Matheus Cardoso <mdonizetecardoso@gmail.com>
2024-10-07 21:48:18 -0300

    Criando conexão com o BD

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const express = require('express')
-const router = express.Router()
+const app = express()
+const connection = require('./database/database')
 
+connection.authenticate().then(() => {
+    console.log("CONN OK!")
+}).catch((MsgErr) => {
+    console.log(MsgErr)
+})
 
+app.listen(8000, () => {
+    console.log("Server ON!")
+})
